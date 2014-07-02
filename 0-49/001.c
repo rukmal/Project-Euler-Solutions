6552c815e478f523f541677788881a6dbd97060c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-07-02 14:56:35 +0530

    Added solution for problem 2

--- 0-49/001.c
+++ 0-49/001.c
@@ -6,18 +6,19 @@
  *
  * For the full collection, see: http://rukmal.github.io/Project-Euler-Solutions
  *
- * This is the solution to problem #001. See: https://projecteuler.net/problem=1
+ * This is the solution to problem #001. See: http://projecteuler.net/problem=1
  */
 
 #include <stdio.h>
 
-int main () {
+int main ()
+{
 	int totalSum = 0;
 	for (int i = 0; i < 1000; i++)
 	{
 		if ((i % 5 == 0) || (i % 3 == 0))
 		{
-			totalSum = totalSum + i;
+			totalSum += i;
 		}
 	}
 	printf("%i\n", totalSum);
